5012dad581db79ba081af81b5fd59ee5e9901d05
Joe Kohlmann <kohlmannj@gmail.com>
2013-06-26 20:01:37 -0500

    Now attaching an additional event listener to disable links in Select2 boxes. This is a problem when users click on the link text to select an item from the Select2 box, which inadvertently causes the browser to follow the link.
    This is the same commit as before, but without the nasty comments in the JavaScript file. Sigh…

--- assets/form_select2.publish.js
+++ assets/form_select2.publish.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function() {
 	// Apply select2 plugin to the select boxes. Select boxes used for the subsectionmanager will be ignored.
 	jQuery('#publish').not('.subsection').find('div.field').not('.field-subsectionmanager, .field-status').find('select').select2();
-
+	jQuery('.select2-choice span, .select2-result-label').on("click", "a", function(e) {
+		e.preventDefault();
+	});
 });
