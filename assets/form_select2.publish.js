bae85b72f219d3dc1beb20952413ba4469142c53
Joe Kohlmann <kohlmannj@gmail.com>
2013-06-28 20:09:26 -0500

    Now removing <option> elements (with no "value" attribute set) from <select> elements, and for single-selection <select> elements, relabeling these empty elements as "[None]" to minimize confusion.

--- assets/form_select2.publish.js
+++ assets/form_select2.publish.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
+	jQuery("select[multiple='multiple'] option:empty").remove();
+	jQuery("select:not([multiple='multiple']) option:empty").text("[None]");
 	// Apply select2 plugin to the select boxes. Select boxes used for the subsectionmanager will be ignored.
 	jQuery('#publish').not('.subsection').find('div.field').not('.field-subsectionmanager, .field-status').find('select').select2();
 	jQuery('.select2-choice span, .select2-result-label').on("click", "a", function(e) {
